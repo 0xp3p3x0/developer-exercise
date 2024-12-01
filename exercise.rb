5b0511fad868418dfac3744745e9e82d89cbb8f5
MITCHURO <luisd.trivino@gmail.com>
2024-11-30 22:05:40 -0500

    completed

--- exercise.rb
+++ exercise.rb
@@ -5,6 +5,18 @@ class Exercise
   # If the word being replaced has a capital first letter, it should instead be replaced with "Marklar".
   def self.marklar(str)
     # TODO: Implement this method
+    str.split.map do |word|
+      if word.gsub(/[^a-zA-Z]/, '').length > 4
+        if word[0] =~ /[A-Z]/
+          replacement = "Marklar"
+        else
+          replacement = "marklar"
+        end
+        word.match?(/[[:punct:]]$/) ? replacement + word[-1] : replacement
+      else
+        word
+      end
+    end.join(' ')
   end
 
   # Return the sum of all even numbers in the Fibonacci sequence, up to
@@ -13,6 +25,18 @@ class Exercise
   # and the sum of its even numbers is (2 + 8) = 10
   def self.even_fibonacci(nth)
     # TODO: Implement this method
+    a, b = 1, 1
+    sum = 0
+    count = 1
+  
+    while count <= nth
+      sum += a if a.even? 
+      a, b = b, a + b     
+      count += 1        
+    end
+
+    sum
+    
   end
 
 end
